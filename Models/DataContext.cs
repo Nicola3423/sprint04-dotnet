8bc62b3c6010a49afb16f0349b70aca5ca9eba79
Nicola3423 <nnmmontecravo@gmail.com>
2025-05-12 14:01:43 -0300

    Primeiro commit

---
+++ Models/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sessions_app.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Paciente> Pacientes { get; set; }
+
+        public DbSet<Medico> Medicos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configuração específica para Oracle
+            modelBuilder.HasDefaultSchema("RM553991");
+
+            // Configuração para sequências Oracle
+            modelBuilder.Entity<Paciente>()
+                .Property(p => p.IdPaciente)
+                .HasDefaultValueSql("PACIENTE_SEQ.NEXTVAL");
+
+            modelBuilder.Entity<Medico>()
+                .Property(m => m.IdMedico)
+                .HasDefaultValueSql("MEDICO_SEQ.NEXTVAL");
+        }
+
+
+    }
+}
